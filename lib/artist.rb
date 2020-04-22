0f0ac5c342eb1cfc6b4389e9c39893e7aba6607b
shamellakin <christlife4u@gmail.com>
2020-04-22 11:59:01 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class Artist 
-  
+  #Has many songs
   attr_accessor :name, :songs
   
   def initialize(name)
@@ -8,16 +8,20 @@ class Artist
     @songs = []
   end 
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
- 
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
+    # select searches through the Song class returns 
+    # an array containing all elements of Songs 
+    #for which the given block returns a true value.
+    #then takes each obj in array and sets the song == to itself(artist)instance
+  end
+
+  def add_song(songs)
+    @songs << songs
+    songs.artist = self
   end
 
   def add_song_by_name(song_name)
-  
+    Song.new(song_name)
   end 
 end
